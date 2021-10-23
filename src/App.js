f24da37b176f81d68310a598d95ca9b6d903db25
Jordan Hughes <jordanhughes421@gmail.com>
2021-10-23 16:17:08 -0400

    added particle animation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
 import Navigation from './screens/navigationBar/NavigationBar'
+import ParticleAnimation from 'react-particle-animation'
+
+
 
 function App() {
   return (
     <div className="App">
         <Navigation />
+        <ParticleAnimation 
+          background={{ r: 21, g: 22, b: 23, a: 1 }}
+          style= {{height: '96vh'}}  
+          particleSpeed= {0.1}
+          particleRadius= {2}
+          color= {{ r: 255, g: 165, b: 0, a: 255 }}
+        />
     </div>
   );
 }
